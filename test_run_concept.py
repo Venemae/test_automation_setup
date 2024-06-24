c2a85bf5f7e9c96b39ba19a9846cda24a0543e93
alandevoteam <alan.ahmad@devoteam.com>
2024-06-24 11:00:13 +0200

    Improved test_python

--- test_run_concept.py
+++ test_run_concept.py
@@ -3,24 +3,15 @@ from playwright.sync_api import Page, expect
 
 
 def test_example(page: Page) -> None:
-    page.goto("https://menzis.nl/zorgvinder")
-    # page.get_by_role("button", name="Accept All Cookies").click()
-    import time
-    time.sleep(3)
+    page.goto("https://zorgvinder.menzis.nl/")
     page.get_by_placeholder("Bijvoorbeeld: Fysiotherapie").click()
-    time.sleep(2)
     page.get_by_placeholder("Bijvoorbeeld: Fysiotherapie").fill("Ziekenhuis")
-    # Simuleer een spatie
-    page.keyboard.press("Space")
-    time.sleep(2)  # even wachten om ervoor te zorgen dat het effect heeft
+    page.get_by_placeholder("Bijvoorbeeld: Fysiotherapie").press('Space')  # Simuleer een spatiebalkdruk
+    page.wait_for_selector('text="Ziekenhuis / ZBC"')  # Wacht tot de tekst "Ziekenhuis / ZBC" zichtbaar is
     page.get_by_text("Ziekenhuis / ZBC").click()
-    time.sleep(3)
     page.get_by_placeholder("Postcode of woonplaats").click()
     page.get_by_placeholder("Postcode of woonplaats").fill("Bedum")
-    # Simuleer een spatie
-    page.keyboard.press("Space")
-    time.sleep(2)  # even wachten om ervoor te zorgen dat het effect heeft
+    page.get_by_placeholder("Postcode of woonplaats").press('Space')  # Simuleer een spatiebalkdruk
     page.get_by_text("Bedum").click()
     page.get_by_role("combobox").select_option("50")
     page.get_by_role("button", name="Zoeken").click()
-    time.sleep(3)
